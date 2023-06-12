3295f67133a1f7f4624183802d0f859127cd49ed
marisaandrade <129205783+marisaandrade@users.noreply.github.com>
2023-06-12 13:09:47 +0100

    codigo

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(1920, 1080);
+  }
+  
+  function draw() {
+    background(220);
+
+    fill(0,191,255)
+    circle (200, 100, 200)
+
+    fill(0,0,0)
+    circle (170, 70, 20)
+
+    fill(0,0,0)
+    circle (220, 70, 20)
+  }
